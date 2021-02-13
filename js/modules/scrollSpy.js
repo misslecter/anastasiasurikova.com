ded6a45b27a00540b169175aeb68a5b954638527
anastasia <anastasia.surikova@akka.eu>
2021-02-13 17:19:45 +0100

    scroll fix + button added

--- js/modules/scrollSpy.js
+++ js/modules/scrollSpy.js
@@ -4,7 +4,8 @@ const sections = document.querySelectorAll('body > section');
 window.addEventListener('scroll', () => {
   sections.forEach((section, key) => {
     const bounding = section.getBoundingClientRect();
-    if (bounding.bottom >= 0 && bounding.bottom <= window.innerHeight) {
+    if ((bounding.bottom >= 0 && bounding.bottom <= window.innerHeight + 100)
+      || (bounding.top >= -window.innerHeight && bounding.top <= 0)) {
       menuLinks.forEach(a => a.classList.remove('active'));
       const link = document.querySelector(`a[href="#${section.id}"]`);
       link.classList.add('active');
